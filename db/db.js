ab8e158244ac36e6bf91315ddd9321f50f2d8cbf
shreyat2712 <shreyat2712@gmail.com>
2024-03-30 22:01:37 +0530

    First commit

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const mongoose =require('mongoose');
 
 async function db() {
   try {
-    await mongoose.connect("mongodb+srv://hunter:hunterxhunter@cluster0.twj9eig.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect(`mongodb+srv://shreyat2712:vGAkcMtQQ9ErcZzt@cluster0.ehhhmxx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
     console.log("Database connected successfully..");
     // mongoose.connection.on('error', (err) => {
     //   console.log('Error connecting to db:', err);
